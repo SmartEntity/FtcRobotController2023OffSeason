897e9b23f2bac524fad75ba55fa3e489dc675b1d
SmartEntity <68695760+SmartEntity@users.noreply.github.com>
2023-03-18 11:14:22 -0400

    Revert "Revert "First push""
    This reverts commit 9bb5c5e2ea2039e6e17f36a80ecc40710364a235.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/objects/State.java
@@ -0,0 +1,5 @@
+package org.firstinspires.ftc.teamcode.objects;
+
+public interface State {
+    double val();
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/movement/MecanumUtil.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.util.movement;
+
+public class MecanumUtil implements DriveUtil {
+}
